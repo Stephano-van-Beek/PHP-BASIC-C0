20cf19ebae9a20dbdc5dfadcceb9fdf916d1e005
Stephano-van-beek <stephano.van.beek@gmail.com>
2021-12-16 09:14:33 +0100

    viarble gemaakt en echo gemaakt

--- 0-Webserver/taak01 - Serveren/index.php
+++ 0-Webserver/taak01 - Serveren/index.php
@@ -1,27 +1,39 @@
 <?php
 <?php
 $txt = "Hallo en Welkom!";
+
+echo "Hallo en Welkom!";
+
 $score = 0;
+
+echo $score;
 
 $naam = "Stéphano";
 $name = "Mijn naam is $naam";
 
+echo $naam;
+
 $_Hello = array("hallo", "hallo2");
+
+echo $_Hello
 
 $leeftijd=19;
 $regelTekst1="Mijn leeftijd is $leeftijd";
 $regelTekst2='Mijn leeftijd is $leeftijd';
 
+echo $regelTekst1;
+echo $regelTekst2;
+
 $melk = 8 euro;
 $melk_prijs="Melk prijs is $melk"
 
+$x = true; // of
+$x = false;
+
+echo $x
+
+$voornaam = "Tom";
+echo gettype($voornaam);
 
 
-
-echo "Hallo en Welkom!";
-echo $score;
-echo $naam;
-echo $_Hello
-echo $regelTekst1;
-echo $regelTekst2;
 ?>
