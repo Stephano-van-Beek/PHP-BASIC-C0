04ccad52fba35b69f3fb33be118be909e80c2a6a
Stephano-van-beek <stephano.van.beek@gmail.com>
2021-12-21 13:15:57 +0100

    array 3 gedaan

--- 3-Arrays/3.3 - Array waarde toevoegen/Index.php
+++ 3-Arrays/3.3 - Array waarde toevoegen/Index.php
@@ -1,5 +1,8 @@
 <?php
 
+$myHeroes
+$myHeroes =  ['Superman', 'Spiderman', 'Batman'];
 
-
+array_push($myHeroes, 'Batman');
+print_r($myHeroes, 'Batman')
 ?>
---
+++ 3-Arrays/3.4 - Multidimensionaal/Index.php
@@ -0,0 +1,8 @@
+<?php
+
+$abcGetallen = [ [a1,a2,a3], ["een", "twee", "drie"], true, false  ];
+
+echo $abcGetallen[0][1];
+print_r($abcGetallen)
+
+?>
