87db67c6617b2ec136b48fab477ca030f53ee0c1
Stephano-van-beek <stephano.van.beek@gmail.com>
2022-01-03 21:08:40 +0100

    5.alles gedaan

---
+++ 5-Conditional_Statements/5.3 - if, else conditional/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    $test1 = 3
+    $test2 = 6
+    
+    if($test1 == $test2) {
+        echo "gelijk";
+    }
+    else {
+        echo "niet gelijk";
+    }
+
+    if($test1 != $test2) {
+        echo "gelijk";
+    }
+    else {
+        echo "niet gelijk";
+    }
+
+    if($test1 === $test2) {
+        echo "gelijk";
+    }
+    else {
+        echo "niet gelijk";
+    }
+
+    if($test1 > $test2) {
+        echo "gelijk";
+    }
+    else {
+        echo "niet gelijk";
+    }
+
+    if($test1 < $test2) {
+        echo "gelijk";
+    }
+    else {
+        echo "niet gelijk";
+    }
+    //is test1 kleiner of gelijk aan test2?
+    if($test1 <= $test2) {
+        echo "gelijk";
+    }
+    else {
+        echo "niet gelijk";
+    }
+    //is test1 groter of gelijk aan test2?
+    if($test1 >= $test2) {
+        echo "gelijk";
+    }
+    else {
+        echo "niet gelijk";
+    }
+
+
+
+
+
+
+
+    ?>
+</body>
+</html>
---
+++ 5-Conditional_Statements/5.4- if, elseif, else conditonal/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    $prod_besteld = 12;
+
+    if($prod_besteld < 10) {
+    $prijs = 1.50;
+    } elseif($prod_besteld < 20) {
+    $prijs = 1.25;
+    } else {
+    $prijs = 1.00;
+    }
+    
+</body>
+</html>
